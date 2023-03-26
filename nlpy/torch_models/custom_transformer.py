61c74bf251168cdfb343dc2a2ead9fb5708bd716
Strabes <Strabes@users.noreply.github.com>
2023-03-26 16:25:15 -0600

    add feedforward parameter sharing to TinyClippedRelativeAttentionTransformer

--- nlpy/torch_models/custom_transformer.py
+++ nlpy/torch_models/custom_transformer.py
@@ -1,16 +1,52 @@
 import torch
 from torch import nn
-from .relative_attention import RelativeClippedAttention
+from .relative_attention import ClippedRelativeAttention
 
-class TinyRelativeClippedAttentionTransformer(nn.Module):
+class TinyClippedRelativeAttentionTransformer(nn.Module):
+    """
+    This model is inspired by the cross-layer parameter sharing approach in
+    ALBERT: A Lite BERT for Self-Supervised Learning of Language Representations
 
-    def __init__(self, d_model, num_heads, num_layers, clip_dist, max_len, share_att_params=False, dropout=0.1):
-        super(TinyRelativeClippedAttentionTransformer, self).__init__()
+    Parameters
+    ----------
+    d_model : int
+        model dimension
+    num_heads : int
+        number of self-attention heads
+        d_model / num_heads must be an integer
+    num_layers : int
+        number of layers
+    clip_dist : int
+        max relative distance for relative position embeddings
+    max_len : int
+        max sequence length of inputs
+    share_att_params: bool, Default: False
+        share attention parameters across layers
+    share_lin_params : bool, Default: False
+        share feed forward parameter across layers
+    dropout : float
+    """
+    def __init__(
+        self,
+        d_model,
+        num_heads,
+        num_layers,
+        clip_dist,
+        max_len,
+        share_att_params=False,
+        share_lin_params=False,
+        dropout=0.1):
+
+        super(TinyClippedRelativeAttentionTransformer, self).__init__()
+        self.d_model = d_model
+        self.num_heads = num_heads
         self.num_layers = num_layers
+        self.clip_dist = clip_dist
+        self.max_len = max_len
         self.batch_norms = nn.ModuleList(
             [nn.BatchNorm1d(num_features=d_model) for i in range(num_layers)])
         self.rga = nn.ModuleList(
-            [RelativeClippedAttention(
+            [ClippedRelativeAttention(
                 d_model=d_model,
                 num_heads=num_heads,
                 clip_dist=clip_dist,
@@ -21,25 +57,37 @@ class TinyRelativeClippedAttentionTransformer(nn.Module):
         for layer in self.rga:
             layer.Er = self.Er
         # if we want to share parameters for keys, queries and values across layers
-        if share_att_params:
-            self.rga_shared_params_key_weight = nn.Parameter(torch.randn(d_model, d_model))
-            self.rga_shared_params_key_bias = nn.Parameter(torch.randn(d_model))
-            self.rga_shared_params_query_weight = nn.Parameter(torch.randn(d_model, d_model))
-            self.rga_shared_params_query_bias = nn.Parameter(torch.randn(d_model))
-            self.rga_shared_params_value_weight = nn.Parameter(torch.randn(d_model, d_model))
-            self.rga_shared_params_value_bias = nn.Parameter(torch.randn(d_model))
-            for layer in self.rga:
-                layer.key.weight = self.rga_shared_params_key_weight
-                layer.key.bias = self.rga_shared_params_key_bias
-                layer.query.weight = self.rga_shared_params_query_weight
-                layer.query.bias = self.rga_shared_params_query_bias
-                layer.value.weight = self.rga_shared_params_value_weight
-                layer.value.bias = self.rga_shared_params_value_bias
+        if share_att_params: self.set_shared_att_params()
+
         self.lin = nn.ModuleList(
             [nn.Linear(
                 in_features=d_model,
                 out_features=d_model) for i in range(num_layers)])
+        # if we want to share parameter across feed forward layers
+        if share_lin_params: self.set_share_lin_params()
     
+    def set_shared_att_params(self):
+        self.rga_shared_params_key_weight = nn.Parameter(torch.randn(self.d_model, self.d_model))
+        self.rga_shared_params_key_bias = nn.Parameter(torch.randn(self.d_model))
+        self.rga_shared_params_query_weight = nn.Parameter(torch.randn(self.d_model, self.d_model))
+        self.rga_shared_params_query_bias = nn.Parameter(torch.randn(self.d_model))
+        self.rga_shared_params_value_weight = nn.Parameter(torch.randn(self.d_model, self.d_model))
+        self.rga_shared_params_value_bias = nn.Parameter(torch.randn(self.d_model))
+        for layer in self.rga:
+            layer.key.weight = self.rga_shared_params_key_weight
+            layer.key.bias = self.rga_shared_params_key_bias
+            layer.query.weight = self.rga_shared_params_query_weight
+            layer.query.bias = self.rga_shared_params_query_bias
+            layer.value.weight = self.rga_shared_params_value_weight
+            layer.value.bias = self.rga_shared_params_value_bias
+
+    def set_share_lin_params(self):
+        self.lin_shared_weight = nn.Parameter(torch.randn(self.d_model, self.d_model))
+        self.lin_shared_bias = nn.Parameter(torch.randn(self.d_model))
+        for layer in self.lin:
+            layer.weight = self.lin_shared_weight
+            layer.bias = self.lin_shared_bias
+
     def forward(self, x):
         # x.shape is (batch_size, seq_len, d_model)
         for i in range(self.num_layers):
@@ -56,14 +104,14 @@ if __name__ == '__main__':
     max_len = 60
     batch_size = 512
     inp = torch.randn(batch_size, max_len, d_model)
-    model = TinyRelativeClippedAttentionTransformer(
+    model = TinyClippedRelativeAttentionTransformer(
         d_model, num_heads, num_layers, clip_dist, max_len, dropout=0.1)
     sum(p.numel() for p in model.parameters() if p.requires_grad)
     # import torch
     # from torch import nn
-    # from nlpy.torch_models.custom_transformer import TinyRelativeClippedAttentionTransformer
+    # from nlpy.torch_models.custom_transformer import TinyClippedRelativeAttentionTransformer
     # should be:
     # self attention: num_layers * num_heads * 3 (q,k,v) *(d_model*(d_model/num_heads) weights + 4 biases) + clip_dist*(d_model/num_heads) clipped emb
-    # linear: 2 layers * (16*16 weights + 16 biases)
+    # linear: num_layers * (16*16 weights + 16 biases)
     # batchnorm: 2 layers * (16 means + 16 variances)
     # 2*4*3*(16*4 + 4) + 7*4 + 2 *(16*16 + 16) + 2*(16+16)
--- nlpy/torch_models/relative_attention.py
+++ nlpy/torch_models/relative_attention.py
@@ -26,6 +26,47 @@ class RelativeAttention(nn.Module):
         )
         # self.mask.shape = (1, 1, max_len, max_len)
 
+    def forward(self, x):
+        # x.shape == (batch_size, seq_len, d_model)
+        batch_size, seq_len, _ = x.shape
+        
+        if seq_len > self.max_len:
+            raise ValueError(
+                "sequence length exceeds model capacity"
+            )
+        
+        k_t = self.key(x).reshape(batch_size, seq_len, self.num_heads, -1).permute(0, 2, 3, 1)
+        # k_t.shape = (batch_size, num_heads, d_head, seq_len)
+        v = self.value(x).reshape(batch_size, seq_len, self.num_heads, -1).transpose(1, 2)
+        q = self.query(x).reshape(batch_size, seq_len, self.num_heads, -1).transpose(1, 2)
+        # shape = (batch_size, num_heads, seq_len, d_head)
+        
+        Er_t = self.get_Er_t(seq_len)
+        # Er_t.shape = (d_head, seq_len)
+        QEr = torch.matmul(q, Er_t)
+        # QEr.shape = (batch_size, num_heads, seq_len, seq_len)
+        Srel = self.skew(QEr)
+        # Srel.shape = (batch_size, num_heads, seq_len, seq_len)
+        
+        QK_t = torch.matmul(q, k_t)
+        # QK_t.shape = (batch_size, num_heads, seq_len, seq_len)
+        attn = (QK_t + Srel) / math.sqrt(q.size(-1))
+        mask = self.mask[:, :, :seq_len, :seq_len]
+        # mask.shape = (1, 1, seq_len, seq_len)
+        attn = attn.masked_fill(mask == 0, float("-inf"))
+        # attn.shape = (batch_size, num_heads, seq_len, seq_len)
+        attn = F.softmax(attn, dim=-1)
+        out = torch.matmul(attn, v)
+        # out.shape = (batch_size, num_heads, seq_len, d_head)
+        out = out.transpose(1, 2)
+        # out.shape == (batch_size, seq_len, num_heads, d_head)
+        out = out.reshape(batch_size, seq_len, -1)
+        # out.shape == (batch_size, seq_len, d_model)
+        return self.dropout(out)
+
+    def get_Er_t(self, seq_len):
+        raise(NotImplementedError("get_Er_t is not implemented"))
+
     def skew(self, QEr):
         # QEr.shape = (batch_size, num_heads, seq_len, seq_len)
         padded = F.pad(QEr, (1, 0))
@@ -42,70 +83,19 @@ class RelativeGlobalAttention(RelativeAttention):
         super(RelativeGlobalAttention).__init__(d_model, num_heads, max_len, dropout)
         self.Er = nn.Parameter(torch.randn(max_len, self.d_head))
 
-    
-    def forward(self, x):
-        # x.shape == (batch_size, seq_len, d_model)
-        batch_size, seq_len, _ = x.shape
-        
-        if seq_len > self.max_len:
-            raise ValueError(
-                "sequence length exceeds model capacity"
-            )
-        
-        k_t = self.key(x).reshape(batch_size, seq_len, self.num_heads, -1).permute(0, 2, 3, 1)
-        # k_t.shape = (batch_size, num_heads, d_head, seq_len)
-        v = self.value(x).reshape(batch_size, seq_len, self.num_heads, -1).transpose(1, 2)
-        q = self.query(x).reshape(batch_size, seq_len, self.num_heads, -1).transpose(1, 2)
-        # shape = (batch_size, num_heads, seq_len, d_head)
-        
+    def get_Er_t(self, seq_len):
         start = self.max_len - seq_len
         Er_t = self.Er[start:, :].transpose(0, 1)
-        # Er_t.shape = (d_head, seq_len)
-        QEr = torch.matmul(q, Er_t)
-        # QEr.shape = (batch_size, num_heads, seq_len, seq_len)
-        Srel = self.skew(QEr)
-        # Srel.shape = (batch_size, num_heads, seq_len, seq_len)
+        return Er_t
         
-        QK_t = torch.matmul(q, k_t)
-        # QK_t.shape = (batch_size, num_heads, seq_len, seq_len)
-        attn = (QK_t + Srel) / math.sqrt(q.size(-1))
-        mask = self.mask[:, :, :seq_len, :seq_len]
-        # mask.shape = (1, 1, seq_len, seq_len)
-        attn = attn.masked_fill(mask == 0, float("-inf"))
-        # attn.shape = (batch_size, num_heads, seq_len, seq_len)
-        attn = F.softmax(attn, dim=-1)
-        out = torch.matmul(attn, v)
-        # out.shape = (batch_size, num_heads, seq_len, d_head)
-        out = out.transpose(1, 2)
-        # out.shape == (batch_size, seq_len, num_heads, d_head)
-        out = out.reshape(batch_size, seq_len, -1)
-        # out.shape == (batch_size, seq_len, d_model)
-        return self.dropout(out)
-        
-    
 
-
-class RelativeClippedAttention(RelativeAttention):
+class ClippedRelativeAttention(RelativeAttention):
     def __init__(self, d_model, num_heads, clip_dist, max_len=1024, dropout=0.1):
-        super(RelativeClippedAttention, self).__init__(d_model, num_heads, max_len, dropout)
+        super(ClippedRelativeAttention, self).__init__(d_model, num_heads, max_len, dropout)
         self.clip_dist = clip_dist
         self.Er = nn.Parameter(torch.randn(clip_dist, self.d_head))
 
-    def forward(self, x):
-        # x.shape == (batch_size, seq_len, d_model)
-        batch_size, seq_len, _ = x.shape
-        
-        if seq_len > self.max_len:
-            raise ValueError(
-                "sequence length exceeds model capacity"
-            )
-        
-        k_t = self.key(x).reshape(batch_size, seq_len, self.num_heads, -1).permute(0, 2, 3, 1)
-        # k_t.shape = (batch_size, num_heads, d_head, seq_len)
-        v = self.value(x).reshape(batch_size, seq_len, self.num_heads, -1).transpose(1, 2)
-        q = self.query(x).reshape(batch_size, seq_len, self.num_heads, -1).transpose(1, 2)
-        # shape = (batch_size, num_heads, seq_len, d_head)
-        
+    def get_Er_t(self, seq_len):
         start = seq_len - self.clip_dist
         if start > 0:
             Er_clipped = self.Er[0,:].repeat(start).reshape(start,-1)
@@ -114,25 +104,4 @@ class RelativeClippedAttention(RelativeAttention):
         else:
             Er_temp = self.Er[start:,:]
         Er_t = Er_temp.transpose(0, 1)
-        # Er_t.shape = (d_head, seq_len)
-        QEr = torch.matmul(q, Er_t)
-        # QEr.shape = (batch_size, num_heads, seq_len, seq_len)
-        Srel = self.skew(QEr)
-        # Srel.shape = (batch_size, num_heads, seq_len, seq_len)
-        
-        QK_t = torch.matmul(q, k_t)
-        # QK_t.shape = (batch_size, num_heads, seq_len, seq_len)
-        attn = (QK_t + Srel) / math.sqrt(q.size(-1))
-        mask = self.mask[:, :, :seq_len, :seq_len]
-        # mask.shape = (1, 1, seq_len, seq_len)
-        attn = attn.masked_fill(mask == 0, float("-inf"))
-        # attn.shape = (batch_size, num_heads, seq_len, seq_len)
-        attn = F.softmax(attn, dim=-1)
-        out = torch.matmul(attn, v)
-        # out.shape = (batch_size, num_heads, seq_len, d_head)
-        out = out.transpose(1, 2)
-        # out.shape == (batch_size, seq_len, num_heads, d_head)
-        out = out.reshape(batch_size, seq_len, -1)
-        # out.shape == (batch_size, seq_len, d_model)
-        return self.dropout(out)
-        
+        return Er_t
